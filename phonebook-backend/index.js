0c7c9a6b980cf34e1a1159dca3844a6335e23163
Amit <amitpeleg007@gmail.com>
2025-11-11 17:49:02 +0200

    Updated API request /info

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,28 +4,7 @@ const Person = require('./models/person')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-// let persons = [
-//         { 
-//             "id": "1",
-//             "name": "Arto Hellas", 
-//             "number": "040-123456"
-//         },
-//         { 
-//             "id": "2",
-//             "name": "Ada Lovelace", 
-//             "number": "39-44-5323523"
-//         },
-//         { 
-//             "id": "3",
-//             "name": "Dan Abramov", 
-//             "number": "12-43-234345"
-//         },
-//         { 
-//             "id": "4",
-//             "name": "Mary Poppendieck", 
-//             "number": "39-23-6423122"
-//         }
-// ]
+
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -35,7 +14,7 @@ app.use(express.static('dist'))
 app.get('/info', (request, response) => {
     const date =  new Date().toLocaleString();
     response.send(`
-        <h1>Phonebook has info for ${persons.length} people</h1>
+        <h1>Phonebook has info for ${Person.countDocuments({})} people</h1>
         <h1>${date}</h1>`
     )
 })
